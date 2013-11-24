8f21fae5f9a0e91e8f78699c193c9158091ab8fc
Marcin Chrost <mchrost@neubloc.net>
2013-11-24 22:51:43 +0100

    primitive http auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :authenticate
+
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic('RESTRICTED ACCESS') do |username, password|
+      username == 'test' && password == 'test'
+    end
+  end
 end
